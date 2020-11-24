eb214d69439307eea34e627669d78614842236f2
JuliadeBerga <56594063+JuliadeBerga@users.noreply.github.com>
2020-11-24 17:16:36 +0100

    Capítol 5
    Capítol 5

--- Basic/Basic/Assets/Scripts/Axe.cs
+++ Basic/Basic/Assets/Scripts/Axe.cs
@@ -2,38 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Axe : MonoBehaviour, IInventoryItem
+public class Axe : InventoryItemBase
 {
-    public string Name
+    public override string Name
     {
         get
         {
             return "Axe";
         }
     }
-    public Sprite _Image = null;
-    public Sprite Image
-    {
-        get
-        {
-            return _Image;
-        }
-    }
-    public void OnPickup()
-    {
-        gameObject.SetActive(false);
-    }
-
-    public void OnDrop()
-    {
-        //Explicat a NavMesh. Utilitzar la càmera que saber quin punt de la pantalla ha tocat el mouse
-        RaycastHit hit = new RaycastHit();
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 1000))
-        {
-            gameObject.SetActive(true);
-            gameObject.transform.position = hit.point;
-        }
-    }
-
 }
